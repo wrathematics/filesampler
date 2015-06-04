95b24f60ff0f629c464049135287a44e873f5a6d
wrathematics <wrathematics@gmail.com>
2015-06-04 18:57:08 -0400

    better docs

--- R/lineSampler-package.r
+++ R/lineSampler-package.r
@@ -1,12 +1,17 @@
 #' File Line Sampler
 #' 
-#' A simple package for reading subsamples of files (primarily csv's)
+#' A simple package for reading subsamples of text files (primarily csv's)
 #' by line in a reasonably efficient manner.  We do so by sampling as the
 #' input file is scanned and randomly choosing whether or not to
-#' dump the current line to an external temporary file.  This is
-#' done in a single pass.
-#'
-#' The heavy lifting is all done in C.
+#' dump the current line to an external temporary file.  This 
+#' temporary file is then read back into R.  For (aggressive) 
+#' downsampling, this is a very effective strategy; for resampling,
+#' you are much better off reading the full dataset into memory.
+#' 
+#' The basic function performs the sampling/reading in a single pass.
+#' There is an alternative version which allows for an exact amout to
+#' be subsampled via reservoir sampling, but this version requires 2
+#' passes through the data.  The heavy lifting is done entirely in C.
 #'
 #' This package, including the underlying C library, is licensed under
 #' the permissive 2-clause BSD license.
