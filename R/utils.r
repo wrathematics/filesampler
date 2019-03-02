42f605d560f956ccbb7eb8f280db4ea2de038f34
wrathematics <wrathematics@gmail.com>
2019-03-01 22:09:17 -0500

    add abspath()

--- R/utils.r
+++ R/utils.r
@@ -1,1 +1,10 @@
 title_case = function(x) gsub(x, pattern="(^|[[:space:]])([[:alpha:]])", replacement="\\1\\U\\2", perl=TRUE)
+
+abspath = function(file)
+{
+  p = path.expand(file)
+  if (!file.exists(p))
+    stop("file does not exist")
+  
+  p
+}
